118e0503dfb85087ffb9c7ab8052a246b19c2da3
Yaroslav <Yaroslavnikolaev2000@gmail.com>
2020-07-31 19:17:31 +0300

    Section About

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const headerDropdown = document.querySelector('.header-bottom__dropdown'),
-        headerMenu = document.querySelector('.header-bottom__navigation');
+        headerMenu = document.querySelector('.header-bottom__navigation')
 
     headerDropdown.addEventListener('click', () => {
         headerMenu.classList.toggle('open');
